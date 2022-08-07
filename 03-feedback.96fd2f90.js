98b119b0d28c2e0e0a08351133ce244156265984
panivnyk <98265791+panivnyk@users.noreply.github.com>
2022-08-07 21:57:34 +0000

    Deploying to gh-pages from @ panivnyk/goit-js-hw-08@c09bf47725b2921e6af186de8899a30f3d00e55a 🚀

--- 03-feedback.96fd2f90.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),t=document.querySelector("input"),o=document.querySelector("textarea"),n="feedback-form-state";function c(){t.textContent=localStorage.getItem(n)||""}console.log(t),console.log(e),console.log(o),c(),e.addEventListener("input",throttle((function(e){}),500))}();
-//# sourceMappingURL=03-feedback.96fd2f90.js.map
